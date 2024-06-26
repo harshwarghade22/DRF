81d656c8339e123dc80766afbfcb243e994d373f
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-06-26 18:48:38 +0530

    Authentication and permissions

--- gs20/api/views.py
+++ gs20/api/views.py
@@ -4,10 +4,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    
-
 
