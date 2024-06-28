29a5f1e43e1c5145e37542836ed55a19d508309c
harshwarghade22 <harshwarghade22@spit.ac.in>
2024-06-28 18:27:39 +0530

    JWT and Simple JWT

--- gs26/gs26/urls.py
+++ gs26/gs26/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
-
+from api.auth import CustomAuthToken
 # Creating Router Object
 router = DefaultRouter()
 
@@ -14,6 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('gettoken/', obtain_auth_token, name='api_token_auth')
+    path('gettoken/', CustomAuthToken.as_view(), name='api_token_auth')
 
 ]
--- gs30/gs30/urls.py
+++ gs30/gs30/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
-
+from api.auth import CustomAuthToken
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 # Creating Router Object
 router = DefaultRouter()
 
@@ -13,7 +14,10 @@ router.register('studentapi', views.StudentModelViewSet, basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('gettoken/', obtain_auth_token, name='api_token_auth')
+    path('gettoken/', TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('refreshtoken/', TokenRefreshView.as_view(),name='token_obtain_pair'),
+    path('verifytoken/',TokenVerifyView.as_view(),name='token_obtain_pair'),
+
 
 ]
+
