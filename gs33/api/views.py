28ca6bd51cb2dfdd9620087b6d026d3c4af18284
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-07-01 17:41:00 +0530

    filtering

---
+++ gs33/api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .serializers import StudentSerializer
+from rest_framework.generics import ListAPIView
+from .models import Student
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        user=self.request.user
+        return Student.objects.filter(passBy=user)
+
+
+
+
+ 
